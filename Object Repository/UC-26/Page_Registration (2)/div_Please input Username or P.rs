cfa122eb8e52948b65d1e72fd935c157f95d02fc
nuBoreZ <kritsanah_supphasri@cmu.ac.th>
2018-03-27 21:38:58 +0700

    UC-26_lecturer_login_invalid_blank_field

---
+++ Object Repository/UC-26/Page_Registration (2)/div_Please input Username or P.rs
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Please input Username or P</name>
+   <tag></tag>
+   <elementGuidId>64d9c121-ceb3-42a8-a570-b9ae5811acd5</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>card-panel red lighten-2</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Please input Username or Password</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;test-swipe-2&quot;)/form[@class=&quot;ng-valid ng-dirty ng-touched&quot;]/div[@class=&quot;card-panel red lighten-2&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
