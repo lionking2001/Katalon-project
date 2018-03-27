cf989a8c60b24dd1b3c785344fdcc36fd8450bfc
nuBoreZ <kritsanah_supphasri@cmu.ac.th>
2018-03-27 22:15:23 +0700

    UC-30_lecturer_blank_assign_grade

---
+++ Object Repository/UC-30/Page_Registration (1)/span_953323 SOFWARE CONS TESTM.rs
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_953323 SOFWARE CONS TESTM</name>
+   <tag></tag>
+   <elementGuidId>7699738a-cf75-4248-83c4-dd1ddf29ac88</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>card-title</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>953323 SOFWARE CONS TEST&amp;MAINT</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/app-root[1]/app-course-detail[1]/body[@class=&quot;grey lighten-4&quot;]/section[@class=&quot;section section-users grey lighten-4&quot;]/div[@class=&quot;container&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;col s12&quot;]/div[@class=&quot;card&quot;]/div[@class=&quot;card-content&quot;]/span[@class=&quot;card-title&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
