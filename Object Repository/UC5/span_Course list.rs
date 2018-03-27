e317c8c16329faa2a7b2b98e3900dce95a3956c6
cheulong <searcheulong@gmail.com>
2018-03-27 22:31:10 +0700

    add UC05-NormalFlow viewCourselist

---
+++ Object Repository/UC5/span_Course list.rs
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_Course list</name>
+   <tag></tag>
+   <elementGuidId>b2f7f204-e523-41ff-9970-18b19fbc41f6</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>card-title</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Course list</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/app-root[1]/app-course-crud[1]/body[@class=&quot;grey lighten-4&quot;]/section[@class=&quot;section section-users grey lighten-4&quot;]/div[@class=&quot;container&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;col s12&quot;]/div[@class=&quot;card&quot;]/div[@class=&quot;card-content&quot;]/span[@class=&quot;card-title&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
