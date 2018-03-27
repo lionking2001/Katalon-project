8f06795a1de4e340716461adc07eb8a65bf62bd7
nuBoreZ <kritsanah_supphasri@cmu.ac.th>
2018-03-27 21:43:43 +0700

    UC-27_lecturer_logout

---
+++ Object Repository/UC-27/Page_Registration/a_Logout.rs
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Logout</name>
+   <tag></tag>
+   <elementGuidId>10496b06-d95c-46ac-8452-3b663c996329</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>/login</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Logout</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/app-root[1]/app-list-course[1]/body[@class=&quot;grey lighten-4&quot;]/nav[@class=&quot;blue darken-2&quot;]/div[@class=&quot;container&quot;]/div[@class=&quot;nav-wrapper&quot;]/ul[@class=&quot;right hide-on-med-and-down&quot;]/li[4]/a[1]</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/UC-27/Page_Registration/span_Courses.rs
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_Courses</name>
+   <tag></tag>
+   <elementGuidId>87d21f1d-d6ca-4fab-ab25-0476b7e5a3ba</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>card-title</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Courses</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/app-root[1]/app-list-course[1]/body[@class=&quot;grey lighten-4&quot;]/section[@class=&quot;section section-users grey lighten-4&quot;]/div[@class=&quot;container&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;col s12&quot;]/div[@class=&quot;card&quot;]/div[@class=&quot;card-content&quot;]/span[@class=&quot;card-title&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
