e31d83534c4841c6a862bc1ce1cbcbc618c7237f
cheulong <searcheulong@gmail.com>
2018-03-27 18:28:13 +0700

    add UC1 Test

---
+++ Object Repository/UC1/Page_Registration/div_Please Fill the Field.rs
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Please Fill the Field</name>
+   <tag></tag>
+   <elementGuidId>1eb495b4-73c1-436c-ab81-63400226a6d5</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>card-panel red lighten-2</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Please Fill the Field</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;panell&quot;)/form[@class=&quot;ng-invalid ng-dirty ng-touched&quot;]/div[3]/div[@class=&quot;card-panel red lighten-2&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/UC1/Page_Registration/div_Username or Password is in.rs
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Username or Password is in</name>
+   <tag></tag>
+   <elementGuidId>21868835-aae7-4db9-a6eb-2303da3427fa</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>card-panel red lighten-2</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Username or Password is incorrect</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;panell&quot;)/form[@class=&quot;ng-dirty ng-touched ng-valid&quot;]/div[3]/div[@class=&quot;card-panel red lighten-2&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
