ea734433dc8fd252e683f15aa42843e8ba903b85
nuBoreZ <kritsanah_supphasri@cmu.ac.th>
2018-03-27 22:44:44 +0700

    UC-31_lecturer_view_summary_grade

---
+++ Object Repository/UC-31/Page_Registration/a_Summary Grade.rs
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Summary Grade</name>
+   <tag></tag>
+   <elementGuidId>9a2f3d25-8951-4500-8bd1-39b96cf549d7</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>routerlink</name>
+      <type>Main</type>
+      <value>/lecturer-summary-grade</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>ng-reflect-router-link</name>
+      <type>Main</type>
+      <value>/lecturer-summary-grade</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>ng-reflect-href</name>
+      <type>Main</type>
+      <value>/lecturer-summary-grade</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>/lecturer-summary-grade</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Summary Grade</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/app-root[1]/app-list-course[1]/body[@class=&quot;grey lighten-4&quot;]/nav[@class=&quot;blue darken-2&quot;]/div[@class=&quot;container&quot;]/div[@class=&quot;nav-wrapper&quot;]/ul[@class=&quot;right hide-on-med-and-down&quot;]/li[2]/a[1]</value>
+   </webElementProperties>
+</WebElementEntity>
